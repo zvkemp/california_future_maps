335737d4d17a96cb97a5766187d85500f7efb923
zvkemp <zvkemp@gmail.com>
2014-06-26 10:31:18 -0700

    condensing function

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ require 'json'
 set :database, { adapter: "sqlite3", database: "cdf_data.sqlite3" }
 
 class Population < ActiveRecord::Base
+  # groups and sums results, combining the given keys and returning an array of hashes
+
+  def self.condense(*keys)
+    keep_columns = column_names - keys.map(&:to_s) - ['id', 'estimate']
+    mask         = -> (row) { Hash[keep_columns.map {|c| [c, row.send(c)] }]}
+    puts mask[first].inspect
+    Hash.new(0).tap do |result|
+      all.each {|row| result[mask[row]] += row.estimate }
+    end.map do |key, estimate|
+      key.merge({ estimate: estimate })
+    end
+  end
 end
 
 METADATA = {
@@ -19,9 +31,22 @@ get '/' do
   "Hello, World"
 end
 
+# /data.json?county=SanFrancisco&race=all&age_group=all&year=2010
 get '/data.json' do
   content_type :json
-  Population.where(params).to_json
+
+  query_params = {}
+  condense_keys = []
+
+  params.each do |key, value|
+    if value == "all"
+      condense_keys << key
+    else
+      query_params[key] = value
+    end
+  end
+
+  Population.where(query_params).condense(*condense_keys).to_json
 end
 
 get '/meta.json' do
